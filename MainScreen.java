abea14fa5e21ef439b5622a251040b60532fa217
dlw1rm4 <kaitlyn.loc101@gmail.com>
2024-12-10 19:25:00 -0800

    Create MainScreen.java

---
+++ MainScreen.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainScreen extends JFrame{
+    public MainScreen() {
+        this.setTitle("Visual Novel");
+		this.setSize(1000, 500);
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("ur mom", SwingConstants.CENTER);
+        panel.setLayout(new BorderLayout());
+        panel.setBackground(new Color(204, 213, 174));
+        panel.add(label, BorderLayout.CENTER);
+
+        this.add(panel);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainScreen();
+    }
+}
